d1d39915220a50babfd5da716f6ae7ac1700b8c9
Innis <1575270674@qq.com>
2023-02-20 07:23:33 +0800

    feat: deploy to fairbase

--- src/statusContainer.js
+++ src/statusContainer.js
@@ -4,7 +4,7 @@ export default class StatusContainer {
     static startTimeStamp = 0;
     static fetchWords() {
         return new Promise((resolve, reject) => {
-            fetch('src/output.json')
+            fetch('/output.json')
                 .then(response => response.json())
                 .then(data => {
                     StatusContainer.getWords = data;
