a61d4a0da6d05028dcf0fa6dfb1d709eb979f410
Innis <1575270674@qq.com>
2023-03-22 12:28:35 +0800

    feat: add icon for mention audio loading done

--- src/components/MessageCard.vue
+++ src/components/MessageCard.vue
@@ -6,25 +6,35 @@
             <div class="message-triangle triangle-left" v-if="role === 'assistant'"></div>
             <div class="message-triangle triangle-right" v-if="role === 'user'"></div>
             <div class="message-box">{{ message }}</div>
+            <Checklist color="#94d82d" :class="audioDoneClass" v-if="audioDoneIconShow" />
         </div>
         <audio :src="audioUrl" controls ref="audio" class="card-audio-tag"></audio>
     </div>
 </template>
 <script>
 import { speechS } from '../Tools';
+import { Checked, Checklist } from '@nutui/icons-vue';
 export default {
+    components: {
+        Checked, Checklist
+    },
     data() {
         return {
             audioUrl: "",
+            audioDoneIconShow: false,
         }
     },
     props: {
         role: String,
         message: String,
+
     },
     computed: {
         containerClass() {
             return this.role === 'user' ? 'message-con-user ' : 'message-con-assistant';
+        },
+        audioDoneClass() {
+            return this.role === 'user' ? 'audio-done-icon-user' : 'audio-done-icon-assistant';
         }
     },
     methods: {
@@ -36,6 +46,9 @@ export default {
         }
     },
     mounted() {
+        this.$refs.audio.addEventListener("loadedmetadata", () => {
+            this.audioDoneIconShow = true;
+        });
         this.getURL();
     }
 }
@@ -46,6 +59,7 @@ export default {
     flex-direction: row;
     justify-content: flex-start;
     margin-bottom: 20px;
+    position: relative;
 }
 
 .message-con-user {
@@ -53,6 +67,7 @@ export default {
     flex-direction: row-reverse;
     justify-content: flex-start;
     margin-bottom: 20px;
+    position: relative;
 }
 
 .avatar {
@@ -94,5 +109,17 @@ export default {
 
 .card-audio-tag {
     display: none;
+}
+
+.audio-done-icon-assistant {
+    position: absolute !important;
+    bottom: 0;
+    right: -20px;
+}
+
+.audio-done-icon-user {
+    position: absolute !important;
+    bottom: 0;
+    left: -20px;
 }
 </style>
